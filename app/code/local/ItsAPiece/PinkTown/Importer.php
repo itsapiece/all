90f018ad9de16605cf89771fcf0e513826810364
Dmitry Fedyuk <dfediuk@gmail.com>
2018-12-05 23:18:43 +0600

    no message

--- app/code/local/ItsAPiece/PinkTown/Importer.php
+++ app/code/local/ItsAPiece/PinkTown/Importer.php
@@ -7,7 +7,18 @@ class Importer {
 	 */
 	static function process() {
 		ini_set('memory_limit', '-1');
-		ini_set('max_execution_time', '0');		
+		ini_set('max_execution_time', '0');
 		Magmi::configure();
+		$f = array_map('str_getcsv',
+			explode("\r\n",
+				str_replace("\n\"", '"',
+					file_get_contents(\Mage::getBaseDir() . '/_my/Drop_Ship_Product_Feed.csv')
+				)
+			)
+		);
+		$keys = array_shift($f);
+		$f = array_filter($f, function($v) {return '' !== $v[0];});
+		file_put_contents(\Mage::getBaseDir('var') . '/log/skus.log', implode("\n", array_column($f, 0)));
+		//xdebug_break();
 	}
 }
